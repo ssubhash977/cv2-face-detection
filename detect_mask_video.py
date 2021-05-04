098ebe580aaf35dea0546f77f69b280e4deb73e3
Subhash S <62792059+ssubhash977@users.noreply.github.com>
2021-05-04 15:39:19 +0530

    Update detect_mask_video.py

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -6,7 +6,7 @@ run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
 
 # Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # To capture video from webcam. 
 cap = cv2.VideoCapture(0)
@@ -26,15 +26,7 @@ while run:
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    # Display
-    #cv2.imshow('img', img)
+    #display
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(img)
 
-    # Stop if escape key is pressed
-    
-        
-# Release the VideoCapture object
-else:
-	st.write('Stopped')
